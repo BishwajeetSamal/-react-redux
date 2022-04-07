8108c914b067b1d7ebccc8b50084644afcea2360
Admin <admin@Admins-iMac-3.local>
2022-04-07 19:54:17 +0530

    multiple reducer

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const redux = require("redux");
 const createStore = redux.createStore;
-
+const combineReducers = redux.combineReducers;
 const BUY_CAKE = "BUY_CAKE";
 const BUY_ICECREAM = "BUY_ICECREAM";
 
@@ -19,19 +19,30 @@ function buyIceCream() {
 
 //(previousState, action)=>newStateclear
 //initialising State
-const initialState = {
+const initialCakeState = {
   numOfCakes: 10,
+};
+
+const initiaIceCreamlState = {
   numOfIceCream: 20,
 };
 
 //defining reducer function
-const reducer = (state = initialState, action) => {
+const cakeReducer = (state = initialCakeState, action) => {
   switch (action.type) {
     case BUY_CAKE:
       return {
         ...state,
         numOfCakes: state.numOfCakes - 1,
       };
+
+    default:
+      return state;
+  }
+};
+
+const iceCreamReducer = (state = initiaIceCreamlState, action) => {
+  switch (action.type) {
     case BUY_ICECREAM:
       return {
         ...state,
@@ -43,7 +54,11 @@ const reducer = (state = initialState, action) => {
   }
 };
 
-const store = createStore(reducer);
+const rootReducer = combineReducers({
+  cake: cakeReducer,
+  iceCream: iceCreamReducer,
+});
+const store = createStore(rootReducer);
 console.log("Initial state", store.getState());
 const unsubscribe = store.subscribe(() => {
   console.log("Updated State", store.getState());
