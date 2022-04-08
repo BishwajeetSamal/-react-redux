b2f216e9cf6b36fb67778a9f955a34bcadbeddb5
Admin <admin@Admins-iMac-3.local>
2022-04-08 13:50:55 +0530

    thunk middleware use

---
+++ ayncActions.js
@@ -0,0 +1,73 @@
+const redux = require("redux");
+const axios = require("axios");
+const thunkMiddleware = require("redux-thunk").default;
+const applyMiddleware = redux.applyMiddleware;
+const createStore = redux.createStore;
+const initialState = {
+  loading: false,
+  users: [],
+  error: "",
+};
+
+const FETCH_USERS_REQUEST = "FETCH_USERS_REQUEST";
+const FETCH_USERS_SUCCESS = "FETCH_USERS_SUCCESS";
+const FETCH_USERS_FAILURE = "FETCH_USERS_FAILURE";
+
+const fetchUsersRequest = () => {
+  return {
+    type: FETCH_USERS_REQUEST,
+  };
+};
+
+const fetchUsersSuccess = (users) => {
+  return {
+    type: FETCH_USERS_SUCCESS,
+    payload: users,
+  };
+};
+
+const fetchUsersFailure = (err) => {
+  return {
+    type: FETCH_USERS_FAILURE,
+    payload: err,
+  };
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case FETCH_USERS_REQUEST:
+      return { ...state, loading: true };
+    case FETCH_USERS_SUCCESS:
+      return {
+        loading: false,
+        users: action.payload,
+        error: "",
+      };
+    case FETCH_USERS_FAILURE:
+      return {
+        loading: false,
+        users: [],
+        error: action.payload,
+      };
+  }
+};
+// note: action creator returns an action.
+//thunk: Thunk middleware is the ability for an action creator to return a function instead of an action object
+const fetchUsers = () => {
+  //This returned function doesn't have to be pure
+  return function (dispatch) {
+    axios
+      .get("https://jsonplaceholder.typicode.com/users")
+      .then((response) => {
+        const users = response.data;
+        dispatch(fetchUsersSuccess(users));
+      })
+      .catch((err) => dispatch(fetchUsersFailure(err.message)));
+  };
+};
+
+const store = createStore(reducer, applyMiddleware(thunkMiddleware));
+store.subscribe(() => {
+  console.log(store.getState());
+});
+store.dispatch(fetchUsers());
